5e60d7ff3ddd0d4e080ece0f2b5f0a3606440dfa
Abhilash <ultaabhi@gmail.com>
2016-09-28 00:13:06 -0700

    Update StringTokenizer.java

--- StringTokenizer.java
+++ StringTokenizer.java
@@ -1,3 +1,7 @@
+Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+
+
+
 import java.util.Scanner;
 
 /**
